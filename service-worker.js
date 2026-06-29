ece419b43c6bf9047553c3e79feb67ec00cf1a06
김재순 <yv_soon@YVSoon-MacBook-Pro.local>
2026-06-29 12:48:11 +0900

    fix: manifest.json, service-worker.js 복구

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'hankkut-routine-v1';
+const ASSETS = [
+  '/youthit-routine/',
+  '/youthit-routine/index.html',
+  '/youthit-routine/manifest.json',
+  '/youthit-routine/icon-192.png',
+  '/youthit-routine/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
